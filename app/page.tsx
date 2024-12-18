1a8da51709a07f2f14409206abc2e985a577cffd
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-12-17 23:08:19 -0300

    starter global css styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
-    <div className="text-3xl">
-      StoreIt - The only storage solution you need.
+    <div className="center">
+      <h1 className="h1">StoreIt - The only storage solution you need.</h1>
     </div>
   );
 }
