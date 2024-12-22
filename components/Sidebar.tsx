b023878237c8fcd9736e653bbfe1964b2e69c58a
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-12-22 20:51:30 -0300

    sidebar markup initial done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,57 @@
+"use client";
+import { navItems } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+  const pathname = usePathname();
+
   return (
-    <div>Sidebar</div>
-  )
+    <aside className="sidebar">
+      <Link href="/">
+        <Image
+          className="hidden h-auto lg:block"
+          src="/assets/icons/logo-full-brand.svg"
+          alt="logo"
+          width={160}
+          height={50}
+        />
+        <Image
+          src="/assets/icons/logo-brand.svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
+      </Link>
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6">
+          {navItems.map(({ url, name, icon }) => (
+            <Link key={name} href={url} className="lg:w-full">
+              <li
+                className={cn(
+                  "sidebar-nav-item",
+                  pathname === url && "shad-active"
+                )}
+              >
+                <Image
+                  className={cn(
+                    "nav-icon",
+                    pathname === url && "nav-icon-active"
+                  )}
+                  src={icon}
+                  alt={name}
+                  width={24}
+                  height={24}
+                />
+                <p className="hidden lg:block">{name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
 }
