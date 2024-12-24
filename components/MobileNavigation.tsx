8f46918bd63502ace5afaa1cd0ee7251430726da
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-12-24 18:48:50 -0300

    initial mobile navigation component

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,7 +1,74 @@
-import React from 'react'
+"use client";
 
-export default function MobileNavigation() {
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@radix-ui/react-separator";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+type Props = {
+  ownerId: string;
+  accountId: string;
+  fullName: string;
+  avatar: string;
+  email: string;
+};
+
+export default function MobileNavigation({
+  ownerId,
+  accountId,
+  fullName,
+  avatar,
+  email,
+}: Props) {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
   return (
-    <div>MobileNavigation</div>
-  )
+    <header className="mobile-header">
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="logo"
+        width={120}
+        height={52}
+        className="h-auto"
+      />
+
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="Search"
+            width={30}
+            height={30}
+          />
+        </SheetTrigger>
+        <SheetContent className="shad-sheet h-screen px-3">
+          <SheetTitle>
+            <div className="header-user">
+              <Image
+                src={avatar}
+                alt="avatar"
+                width={44}
+                height={44}
+                className="header-user-avatar"
+              />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullName}</p>
+                <p className="caption">{email}</p>
+              </div>
+            </div>
+            <Separator className="mb-4 bg-light-200/20" />
+          </SheetTitle>
+          <SheetDescription>aaaaaaaaaaaaaaaaaaaaaaaa</SheetDescription>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 }
