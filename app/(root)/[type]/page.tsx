3d17a6d26732fb07e101aeac2c709e0d99045095
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-12-31 04:25:10 -0300

    added type dinamic route

---
+++ app/(root)/[type]/page.tsx
@@ -0,0 +1,13 @@
+import { SearchParamProps } from "@/types";
+
+export default async function Page({ params }: SearchParamProps) {
+  const type = ((await params)?.type as string) || "";
+
+  return (
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
+      </section>
+    </div>
+  );
+}
